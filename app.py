f41afc9a3c5ec313b0b2df0980d88e0104fcaf85
Michael Long <michael.long0409@gmail.com>
2020-07-11 22:04:09 +0100

    Add translation functionality and remove case sensitivity

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ import json
 with open("data.json") as file:
     data = json.load(file)
 
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    else:
+        return "No such word exists. Please double check your input."
+
+word = input("Please enter a word to translate: ")
+print(translate(word))
