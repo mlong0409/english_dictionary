b28790f728caf8da874817b2c7cc3d29d2ac6291
Michael Long <michael.long0409@gmail.com>
2020-07-12 03:29:56 +0100

    made the output more user friendly

--- app.py
+++ app.py
@@ -20,4 +20,9 @@ def translate(word):
             return "No such word exists. Please double check your input."
 
 word = input("Please enter a word to translate: ")
-print(translate(word))
+output = translate(word)
+if type(output) is list:
+    for definition in output:
+        print(definition)
+else:
+    print(output)
